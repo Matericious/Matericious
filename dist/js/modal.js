51bc77afd32a14816169d6bbe837f27b3d1d4cb0
Kascique Lowmans <kascique@themastercoder.com>
2018-10-03 20:35:13 -0400

    Modal JavaScript

---
+++ dist/js/modal.js
@@ -0,0 +1,100 @@
+function Modal(data, callback) {
+  if (!data.pos) {
+    data["pos"] = {
+      vertical: "center",
+      horizontal: "center"
+    };
+  }
+
+  if (!data.theme) data.theme = "";
+
+  if (!data.title) data.title = "Modal";
+
+  if (!data.message) data.message = "This is the awesome modal you can use to show cool messages!";
+  
+  if(!data.overlayStyle) data.overlayStyle = "";
+
+  if (data.overlay === false) {
+     data.overlay = "";
+  } else {
+     data.overlay = "overlay";
+  }
+
+  var modalElem = $getT("modal-con", 0);
+  modalElem.innerHTML = "";
+  modalElem.className = "";
+
+  
+  if(data.primary == "") data.primary = "OK";
+  
+  if(!data.secondary || data.secondary == "") data.secondary = "";
+  
+  if(data.title == "") data.title = "modal";
+  
+  if(!data.modalStyle) data.modalStyle = 'ocean';
+  
+  if(data.message == "") data.message = "";
+  
+  var messageModalHTML = 
+      '<modal class="'+data.modalStyle+'">'+
+      '<header>'+data.title+'</header><main>'+data.message+'</main>'+
+      '<action><button class="actionTwo">'+data.secondary+'</button><button class="actionOne">'+data.primaryButton+'</button></action></modal>';
+
+  var modalClass =
+    data.overlay +
+    " " +
+    data.pos.vertical +
+    " " +
+    data.pos.horizontal +
+    " " +
+    data.theme + 
+    " " + 
+    data.overlayStyle;
+  
+  if(data.type = 'message'){
+     modalElem.innerHTML = messageModalHTML;
+  }
+  modalElem.className += modalClass;
+  
+  $getT("modal-con", 0).className += " fadeIn";
+  $getT("modal", 0).className += " slideDownIn";
+  
+  $getCL("actionTwo", 0).addEventListener("click", function() {
+     close();
+     callback(false);
+  });
+  $getCL("actionOne", 0).addEventListener("click", function() {
+    close();     
+    callback(true);
+  }); 
+  
+  function timer(time){
+    var timer;
+    if(time != null){
+       timer = setInterval(function(){
+          clearInterval(timer);
+          close();
+       },time);
+     }
+  }
+  
+  function close(){
+     $getT("modal-con", 0).classList.remove("fadeIn");
+     $getT("modal", 0).classList.remove("slideDownIn");
+  }
+
+  timer(data.time);
+}
+
+/*this function returns the element by ID*/
+function $getID(e) {
+  return document.getElementById(e);
+}
+/*this function returns the element by class name*/
+function $getCL(e, n) {
+  return document.getElementsByClassName(e)[n];
+}
+/*this function returns the element by tag name*/
+function $getT(e, n) {
+  return document.getElementsByTagName(e)[n];
+}
