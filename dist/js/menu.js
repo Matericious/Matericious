1bf77be66fbabed6d9e94765d3c6414103f4faa5
Kascique Lowmans <kascique@themastercoder.com>
2018-10-06 10:00:23 -0400

    Create menu.js

---
+++ dist/js/menu.js
@@ -0,0 +1,50 @@
+function Menu(id){
+  var clickTimes = 0,
+      targetBtn = $get('[menu='+id+']'),
+      targetMenu = $getID(id),
+      default_class = targetMenu.className;
+  
+ // alert(targetBtn.getAttribute("menu"));
+  
+  targetMenu.className = "pullDown";
+  
+  var pos = [targetBtn.offsetTop, targetBtn.offsetLeft];
+    
+  targetMenu.style.top = pos[0]+"px";
+  targetMenu.style.left = pos[1]+"px";
+    
+  document.addEventListener("click", function(event){
+    clickTimes++;
+   // if (!event.target.closest(id)) {
+      if(clickTimes > 1){
+        clickTimes = 0;
+        close(targetMenu);
+      }
+   // };
+  });
+  
+  function close(targetMenu){
+    targetMenu.className = " ";
+  }
+}
+
+/*this function returns the element by ID*/
+function $getID(e) {
+  return document.getElementById(e);
+}
+/*this function returns the element by class name*/
+function $getCL(e, n) {
+  return document.getElementsByClassName(e)[n];
+}
+/*this function returns the element by tag name*/
+function $getT(e, n) {
+  return document.getElementsByTagName(e)[n];
+}
+
+function $get(e){
+  return document.querySelector(e)
+}
+
+function $getAll(e){
+  return document.querySelectorAll(e)
+}
