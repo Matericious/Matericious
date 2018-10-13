e14ebc6c1351f7115f3909584c977a74a754708d
Kascique Lowmans <kascique@themastercoder.com>
2018-10-13 08:31:09 -0400

    Add missing function

--- dist/js/drawer.js
+++ dist/js/drawer.js
@@ -79,6 +79,13 @@ function $get(e) {
   return document.querySelector(e);
 }
 
+function hasClass(el, className) {
+  if (el.classList)
+    return el.classList.contains(className)
+  else
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+}
+
 function $addClass(el, className) {
   if (el.classList) el.classList.add(className);
   else if (!hasClass(el, className)) el.className += " " + className;
