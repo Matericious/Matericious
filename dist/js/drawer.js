053bab63147a954ce66f6cf65f2a0f03b86ed517
Kascique Lowmans <kascique@themastercoder.com>
2018-10-13 08:14:15 -0400

    Restructure drawer js

--- dist/js/drawer.js
+++ dist/js/drawer.js
@@ -1,52 +1,57 @@
 ready(function() {
-  var _drawer = $get(".drawer"),
+  var drawer = function() {
+    var _drawer = $get(".drawer"),
       _doc = $get("body");
-  if(_drawer.className.includes("active")){ 
-    _drawer.style.left = 0 + "px";
-    _doc.style.marginLeft = _drawer.offsetWidth + "px";
-  }
-  if (_drawer.className.includes("permanent")) {
-    $addClass(_drawer, "notrans");
-    $addClass(_doc, "notrans");
-    _drawer.style.left = 0 + "px";
-    _doc.style.marginLeft = _drawer.offsetWidth + "px";
-  } else {
-    call("[drawer]", "click", function() {
-      var _navType = this.getAttribute("drawer");
-      var clickTimes = 0;
-      var _drawer = $get(".drawer"),
-        _doc = $get("body");
+    if (_drawer.className.includes("active")) {
+      _drawer.style.left = 0 + "px";
+      _doc.style.marginLeft = _drawer.offsetWidth + "px";
+    }
+    if (_drawer.className.includes("permanent")) {
+      $addClass(_drawer, "notrans");
+      $addClass(_doc, "notrans");
+      _drawer.style.left = 0 + "px";
+      _doc.style.marginLeft = _drawer.offsetWidth + "px";
+      return;
+    } else {
+      call("[drawer]", "click", function() {
+        var _navType = this.getAttribute("drawer");
+        var clickTimes = 0;
+        var _drawer = $get(".drawer"),
+          _doc = $get("body");
 
-      if (_drawer.style.left != 0 + "px") {
-        if (_navType == "overlay") {
-          _drawer.style.left = 0 + "px";
-          $addClass(_doc, "overlay");
-          $addClass(_doc, "fadeIn");
-          document.addEventListener("click", clickOutSide, true);
-        } else if (_navType == "push") {
-          _drawer.style.left = 0 + "px";
-          _doc.style.marginLeft = _drawer.offsetWidth + "px";
+        if (_drawer.style.left != 0 + "px") {
+          if (_navType == "overlay") {
+            _drawer.style.left = 0 + "px";
+            $addClass(_doc, "overlay");
+            $addClass(_doc, "fadeIn");
+            document.addEventListener("click", clickOutSide, true);
+          } else if (_navType == "push") {
+            _drawer.style.left = 0 + "px";
+            _doc.style.marginLeft = _drawer.offsetWidth + "px";
+          }
+        } else {
+          close();
         }
-      } else {
-        close();
-      }
 
-      function clickOutSide() {
-        clickTimes++;
-        close();
-      }
+        function clickOutSide() {
+          clickTimes++;
+          close();
+        }
 
-      function close() {
-        _drawer.style.left = "-" + 100 + "%";
-        $addClass(_doc, "fadeOut");
-        $removeClass(_doc, "fadeIn");
-        $removeClass(_doc, "fadeOut");
-        $removeClass(_doc, "overlay");
-        _doc.style.marginLeft = 0 + "px";
-        document.removeEventListener("click", clickOutSide, true);
-      }
-    });
-  }
+        function close() {
+          _drawer.style.left = "-" + 100 + "%";
+          $addClass(_doc, "fadeOut");
+          $removeClass(_doc, "fadeIn");
+          $removeClass(_doc, "fadeOut");
+          $removeClass(_doc, "overlay");
+          _doc.style.marginLeft = 0 + "px";
+          document.removeEventListener("click", clickOutSide, true);
+          return;
+        }
+      });
+    }
+  };
+  drawer();
 });
 
 function ready(callback) {
