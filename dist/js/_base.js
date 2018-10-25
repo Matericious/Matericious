76e347141b65b9e2657664af09f8a4c4621d2884
Kascique Lowmans <kascique@themastercoder.com>
2018-10-25 09:21:52 -0400

    Create base for all js code

---
+++ dist/js/_base.js
@@ -0,0 +1,66 @@
+function ready(callback) {
+  // in case the document is already rendered
+  if (document.readyState != "loading") callback();
+  else if (document.addEventListener)
+    // modern browsers
+    document.addEventListener("DOMContentLoaded", callback);
+  else
+    // IE <= 8
+    document.attachEvent("onreadystatechange", function() {
+      if (document.readyState == "complete") callback();
+    });
+}
+
+function call($class, $event, $func) {
+  var elems = document.querySelectorAll($class);
+  for (i = 0; i < elems.length; ++i) {
+    elems[i].addEventListener($event, $func);
+  }
+}
+
+function $get(e) {
+  return document.querySelector(e);
+}
+
+function $all(e) {
+  return document.querySelectorAll(e);
+}
+
+function hexToRgb(hex) {
+  var bigint = parseInt(hex, 16);
+  var r = (bigint >> 16) & 255;
+  var g = (bigint >> 8) & 255;
+  var b = bigint & 255;
+  return r + "," + g + "," + b;
+}
+
+function hasClass(el, className) {
+  if (el.classList)
+    return el.classList.contains(className)
+  else
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+}
+
+function $addClass(el, className) {
+  if (el.classList) el.classList.add(className);
+  else if (!hasClass(el, className)) el.className += " " + className;
+}
+
+function $removeClass(el, className) {
+  if (el.classList) el.classList.remove(className);
+  else if (hasClass(el, className)) {
+    var reg = new RegExp("(\\s|^)" + className + "(\\s|$)");
+    el.className = el.className.replace(reg, " ");
+  }
+}
+
+function addEvent(object, type, callback) {
+    if (object == null || typeof(object) == 'undefined') return;
+    if (object.addEventListener) {
+        object.addEventListener(type, callback, false);
+    } else if (object.attachEvent) {
+        object.attachEvent("on" + type, callback);
+    } else {
+        object["on"+type] = callback;
+    }
+}
