83efbdecec3f8ffa716f1ea9b846a8e3321aed9a
Kascique Lowmans <kascique.lowmans@gmail.com>
2018-11-30 15:45:17 -0400

    Feature added to loader
    > Can now build a loader from JS

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,6 +1,7 @@
 //=include _base.js
 
 function loader(data) {
+  data = (!data) ? '' : data;
   this.id = is_string(data) ? data : data.target;
   this.data = data;
   
@@ -14,6 +15,7 @@ function loader(data) {
       }, time);
     }
   };
+  
   this.close = ()=>{
     var id = $get(this.id);
     $removeClass(id, 'slideDownIn');
@@ -23,13 +25,28 @@ function loader(data) {
     return (!che) ? def : che;
   };
   
-  this.build = ()=>{};
-  
   this.open = (time)=>{
     this.id = ((this.id.substring(0) == '#') ? '' : '#')+this.id;
     alert(this.id);
     var id = $get(this.id);
     $addClass(id, 'slideDownIn');
     this.$timer(time, this.close);
+  };
+  
+  this.build = (time)=> {
+    let name = this.id = 'sys_gen_loader_id',
+        size = this.is(this.data.size, 'small'),
+        type = this.is(this.data.type, 'circular'),
+        pos = [this.is(this.data.vertical, 'bottom'), this.is(this.data.horizontal, 'right')],
+        title = (size == 'large') ? this.is(this.data.title, 'Please wait') : null,
+        subtext = (size == 'large') ? this.is(this.data.subtext, 'This page is loading') : null;
+
+    let small_template = '<div id="'+name+'" class="loader small"><progress class="'+type+'"/></div>',
+        large_template = '<div id="'+name+'" class="loader large"><label class="title">'+title+'</label><span class="subtext">'+subtext+'</span> <progress class="'+type+'"/></div>',
+        template = (size == 'small') ? small_template : large_template;
+
+    $get('body').innerHTML += template;
+      alert("pass");
+      this.open(time);
   };
 }
