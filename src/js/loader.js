a81d44bc876d04f3d5449213e02c2360a4b2f286
Kascique Lowmans <kascique.lowmans@gmail.com>
2018-12-01 08:50:01 -0400

    Update loader.js

--- src/js/loader.js
+++ src/js/loader.js
@@ -37,11 +37,12 @@ function loader(data) {
         size = this.is(this.data.size, 'small'),
         type = this.is(this.data.type, 'circular'),
         pos = [this.is(this.data.vertical, 'bottom'), this.is(this.data.horizontal, 'right')],
-        title = (size == 'large') ? this.is(this.data.title, 'Please wait') : null,
-        subtext = (size == 'large') ? this.is(this.data.subtext, 'This page is loading') : null;
+        title = (size == 'large') ? this.is(this.data.title, 'Please wait') : '',
+        subtext = (size == 'large' && type == 'circular') ? this.is(this.data.subtext, 'This page is loading') : '',
+        _class = pos[0]+' '+pos[1] +' ' + ((type == 'linear') ? 'lin' : '');
 
-    let small_template = '<div id="'+name+'" class="loader small"><progress class="'+type+'"/></div>',
-        large_template = '<div id="'+name+'" class="loader large"><label class="title">'+title+'</label><span class="subtext">'+subtext+'</span> <progress class="'+type+'"/></div>',
+    let small_template = '<div id="'+name+'" class="loader small '+_class+'"><progress class="'+type+'"/></div>',
+        large_template = '<div id="'+name+'" class="loader large '+_class+'"><label class="title">'+title+'</label><span class="subtext">'+subtext+'</span> <progress class="'+type+'"/></div>',
         template = (size == 'small') ? small_template : large_template;
 
     $get('body').innerHTML += template;
