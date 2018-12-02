8a9eb6d3a4732bf66eab858b87bd7ad67fef603e
kascique <kascique.lowmans@gmail.com>
2018-12-02 00:07:55 -0400

    Missing files for v0.9.0

---
+++ js/loader.js
@@ -0,0 +1,142 @@
+/**
+ * Matericious v0.9.0 (https://matericious.com/)
+ * Copyright 2018 Matericious Authors
+ * Licensed under MIT (https://github.com/Matericious/Matericious/blob/master/LICENSE)
+ */
+
+"use strict";
+
+function ready(callback) {
+  if (document.readyState != "loading") callback();else if (document.addEventListener) 
+    document.addEventListener("DOMContentLoaded", callback);else 
+    document.attachEvent("onreadystatechange", function () {
+      if (document.readyState == "complete") callback();
+    });
+}
+
+function call($class, $event, $func) {
+  var elems = document.querySelectorAll($class);
+
+  for (var i = 0; i < elems.length; ++i) {
+    addEvent(elems[i], $event, $func);
+  }
+}
+
+function $handle(callback) {
+  try {
+    callback();
+  } catch (err) {}
+}
+
+function $getChild(parent, child, n) {
+  return document.querySelectorAll(parent + ' ' + child)[n];
+}
+
+function $get(e) {
+  return document.querySelector(e);
+}
+
+function $all(e) {
+  return document.querySelectorAll(e);
+}
+
+function hexToRgb(hex) {
+  var bigint = parseInt(hex, 16);
+  var r = bigint >> 16 & 255;
+  var g = bigint >> 8 & 255;
+  var b = bigint & 255;
+  return r + "," + g + "," + b;
+}
+
+function hasClass(el, className) {
+  $handle(function () {
+    if (el.classList) return el.classList.contains(className);else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+  });
+}
+
+function $addClass(el, className) {
+  if (el.classList) el.classList.add(className);else if (!hasClass(el, className)) el.className += " " + className;
+}
+
+function $removeClass(el, className) {
+  if (el.classList) el.classList.remove(className);else if (hasClass(el, className)) {
+    var reg = new RegExp("(\\s|^)" + className + "(\\s|$)");
+    el.className = el.className.replace(reg, " ");
+  }
+}
+
+function addEvent(object, type, callback) {
+  if (object == null || typeof object == 'undefined') return;
+
+  if (object.addEventListener) {
+    object.addEventListener(type, callback, false);
+  } else if (object.attachEvent) {
+    object.attachEvent("on" + type, callback);
+  } else {
+    object["on" + type] = callback;
+  }
+}
+
+function is_string(data) {
+  if (typeof data === "string" || data instanceof String) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function loader(data) {
+  var _this = this;
+
+  data = !data ? '' : data;
+  this.id = is_string(data) ? data : data.target;
+  this.data = data;
+
+  this.$timer = function (time, callback) {
+    var id = _this.id;
+    var timer;
+
+    if (time != null) {
+      timer = setInterval(function () {
+        clearInterval(timer);
+        callback(id);
+      }, time);
+    }
+  };
+
+  this.close = function () {
+    var id = $get(_this.id);
+    $removeClass(id, 'slideDownIn');
+  };
+
+  this.is = function (che, def) {
+    return !che ? def : che;
+  };
+
+  this.open = function (time) {
+    _this.id = (_this.id.charAt(0) == '#' ? '' : '#') + _this.id;
+    var id = $get(_this.id);
+    $addClass(id, 'slideDownIn');
+
+    _this.$timer(time, _this.close);
+  };
+
+  this.build = function (time) {
+    var name = _this.id = 'sys_gen_loader_id',
+        size = _this.is(_this.data.size, 'small'),
+        type = _this.is(_this.data.type, 'circular'),
+        pos = [_this.is(_this.data.pos.vertical, ''), _this.is(_this.data.pos.horizontal, '')],
+        title = size != 'small' ? _this.is(_this.data.title, 'Please wait') : '',
+        subtext = size == 'large' && type == 'circular' ? _this.is(_this.data.subtext, 'This page is loading') : '',
+        theme = _this.is(_this.data.theme, ''),
+        _class = pos[0] + ' ' + pos[1] + ' ' + (type == 'linear' ? 'lin' : '') + ' ' + theme;
+
+    var small_template = '<div id="' + name + '" class="loader small ' + _class + '"><progress class="' + type + '"/></div>',
+        large_template = '<div id="' + name + '" class="loader large ' + _class + '"><label class="title">' + title + '</label><span class="subtext">' + subtext + '</span> <progress class="' + type + '"/></div>',
+        base_template = '<div id="' + name + '" class="loader base ' + _class + '"><label class="title">' + title + '</label><progress class="circular"/></div>',
+        template = size == 'small' ? small_template : size == 'base' ? base_template : large_template;
+    $get('body').innerHTML += template;
+
+    _this.open(time);
+  };
+}
