d24b7ceebf10497121d8d700462bb87d52c91a50
Kascique Lowmans <kascique.lowmans@gmail.com>
2018-12-20 11:01:25 -0400

    Improve Bottom navigation component

--- src/js/bottom_navigation.js
+++ src/js/bottom_navigation.js
@@ -2,14 +2,18 @@
 
 function BottomNavigation() {
   call(".bottomNav > [nav-id]", "click", function() {
-    let target = this.getAttribute("nav-id"),
-      btn = $get(`[nav-id='${target}']`);
+    var target = this.getAttribute("nav-id"),
+      btn = $get("[nav-id='"+target+"']");
+  
+    var divs = $all(".bottomNav > [nav-id]");
+    var contents = $all("[nav-content]");
     
-    let divs = document.querySelectorAll(".bottomNav > [nav-id]");
-
     [].forEach.call(divs, function(el) {
        $removeClass(el, 'active');
     });
+    
+    [].forEach.call(contents, function(el) { $removeClass(el, 'active'); });
+    
     $addClass(btn, 'active');
   });
 }
