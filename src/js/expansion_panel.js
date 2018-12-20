a3845f3589c74e49e9483c727ed511391a554148
Kascique Lowmans <kascique.lowmans@gmail.com>
2018-12-20 11:03:25 -0400

    Improve Expansion Panel

--- src/js/expansion_panel.js
+++ src/js/expansion_panel.js
@@ -1,21 +1,25 @@
 //=include _base.js
 
 function ExpansionPanel() {
-  call(".panel > [expan]", "click", function () {
-    let target = this.getAttribute("expan"),
-      details = $get(`#${target}`),
-      panel = $get(`.panel > [expan="${target}"]`),
-      contentHeight = $get("#" + target + " > .content").offsetHeight,
-      icon = $get(`[expan="${target}"] > i`),
-      cusIcons = icon.getAttribute("cus-icon");
-    let icons = ['keyboard_arrow_down', 'keyboard_arrow_up'];
-    if (panel.className.includes("active")) {
+  call(".panel > [expan]", "click", function() {
+    var target = this.getAttribute("expan"),
+        details = $get("#"+target),
+        panel = $get(".panel > [expan="+target+"]"),
+        contentHeight = $get("#"+target+" > .content").offsetHeight,
+        icon = $get("[expan="+target+"] > i"),
+        cusIcon = (!icon.getAttribute("cus-icon")) ? 'keyboard_arrow_up' : icon.getAttribute("cus-icon");
+    
+    var icons = ['keyboard_arrow_down', cusIcon];
+    
+    if(panel.className.includes("active")){
       icon.innerHTML = icons[0];
-      details.style.height = 0 + "px";
+     // icon.setAttribute("cus-icon", icons[1]);
+      details.style.height = 0+"px";
       $removeClass(panel, 'active');
-    } else {
+    }else{
       icon.innerHTML = icons[1];
-      details.style.height = contentHeight + "px";
+     // icon.setAttribute("cus-icon", icons[0]);
+      details.style.height = contentHeight+"px";
       $addClass(panel, 'active');
     }
   });
