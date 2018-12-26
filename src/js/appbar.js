7979d37576f681cf10eab47c48dd64e1875bd78c
Kascique Lowmans <kascique.lowmans@gmail.com>
2018-12-26 15:51:43 -0400

    Fix issue with nesting menu

--- src/js/appbar.js
+++ src/js/appbar.js
@@ -1,47 +1,56 @@
 //=include _base.js
 
 function nesting() {
-  const large_screen = 992,
-    medium_screen = 768,
-    small_screen = 500;
+  var large_screen = 992,
+      medium_screen = 768,
+      small_screen = 500;
 
   function checkNesting() {
-    let w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    let num_to_nest;
-    let nest_menu = $get("#nesting");
-    let nest_menu_btn = $get('[open-menu="nesting"]');
-    let nest_btns = $all(".appbar .nest");
+    var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var num_to_nest;
+    var nest_menu = $get("#nesting");
+    var nest_menu_btn = $get('[open-menu="nesting"]');
+    var nest_btn = $all(".appbar .nest");
+
     if (w < large_screen) {
       num_to_nest = 1;
+
       if (w <= medium_screen && w >= small_screen) {
         num_to_nest = 2;
       } else if (w <= small_screen) {
-        num_to_nest = nest_btns.length;
+        num_to_nest = nest_btn.length;
       }
     }
-    nest_btns.forEach(function (nest_btn) {
-      nest_btn.style.display = "block";
-    })
+
+    for (var c = 0; c < nest_btn.length; c++) {
+      nest_btn[c].style.display = "inline-block";
+    }
+
     nest_menu.innerHTML = "";
     nest_menu_btn.style.display = "none";
-    nest_btns.forEach(function (nest_btn) {
-      let icon_btn_title = nest_btn.getAttribute("title");
-      let icon_title;
+
+    for (var c = 0; c < num_to_nest; c++) {
+      var icon_btn_title = nest_btn[c].getAttribute("title");
+      var icon_title;
+
       if (icon_btn_title != null) {
-        icon_title = ` ${icon_btn_title}`;
+        icon_title = " " + icon_btn_title;
       } else {
         icon_title = "";
       }
-      nest_menu_btn.style.display = "inline";
-      nest_btn.style.display = "none";
-      nest_menu.innerHTML += `<li><a href="#">${nest_btn.innerHTML + icon_title}</a></li>`;
-    })
+
+      nest_menu_btn.style.display = "inline-block";
+      nest_btn[c].style.display = "none";
+      nest_menu.innerHTML += '<li><a href="#">' + nest_btn[c].innerHTML + icon_title + "</a></li>";
+    }
   }
+
   checkNesting();
   addEvent(window, "resize", function () {
     checkNesting();
   });
 }
+
 
 function collapse() {
   let $curr_scr_pos = window.pageYOffset;
