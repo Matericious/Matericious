29b88f4798857a5118c63cc2ccf05523ca990619
Shamshid <shmshd12@gmail.com>
2018-12-30 00:13:49 +0400

    added lint tasks for sass and js and improved gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,112 @@
-const gulp = require('gulp'),
-path = require('path'),
-sass = require('gulp-sass'),
-autoprefixer = require('gulp-autoprefixer'),
-sourcemaps = require('gulp-sourcemaps'),
-header = require('gulp-header-comment'),
-strip = require('gulp-strip-comments'),
-uglify = require('gulp-uglify'),
-concat = require('gulp-concat'),
-babel = require('gulp-babel'),
-include = require('gulp-include');
+const {src, task, dest, watch, series} = require("gulp"),
+path = require("path"),
+sass = require("gulp-sass"),
+autoprefixer = require("gulp-autoprefixer"),
+sourcemaps = require("gulp-sourcemaps"),
+header = require("gulp-header-comment"),
+strip = require("gulp-strip-comments"),
+uglify = require("gulp-uglify"),
+concat = require("gulp-concat"),
+babel = require("gulp-babel"),
+include = require("gulp-include"),
+sasslint = require("gulp-sass-lint"),
+eslint = require("gulp-eslint")
 
-var autoprefixer_options={
-  browsers: '> 5%, last 2 versions, Firefox ESR',
+let autoprefixerOptions = {
+  browsers: "> 5%, last 2 versions, Firefox ESR",
   cascade: false
-};
+},
+dirSass = "src/scss/**/*.scss",
+dirEs = ["src/js/_base.js", "src/js/*.js"]
 
-gulp.task('compilecss',(cb)=>{
-  return gulp.src('src/scss/**/*.scss')
-    .pipe(sass({outputStyle:'expanded'})
-      .on('error',function(err){cb(err);}))
-    .pipe(autoprefixer(autoprefixer_options))
-    .pipe(header({file:path.join(__dirname, 'src/info.txt')}))
-    .pipe(gulp.dest('css/'));
-});
+task("lintsass", (cb) => {
+  return src(dirSass)
+    .pipe(sasslint())
+    .pipe(sasslint.format())
+    .pipe(sasslint.failOnError())
+})
 
-gulp.task('combinecss',(cb)=>{
-  return gulp.src('src/scss/**/*.scss')
-    .pipe(concat('matericious.css'))
+task("lintjs", () => {
+  return src(dirEs)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+})
+
+task("compilecss", (cb) => {
+  return src(dirSass)
+    .pipe(sass({outputStyle: "expanded"})
+      .on("error", (err) => {
+        cb(err)
+      }))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(header({file: path.join(__dirname, "src/info.txt")}))
+    .pipe(dest("css/"))
+})
+
+task("combinecss", (cb) => {
+  return src(dirSass)
+    .pipe(concat("matericious.css"))
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle:'expanded'})
-      .on('error',function(err){cb(err);}))
-    .pipe(autoprefixer(autoprefixer_options))
-    .pipe(header({file:path.join(__dirname, 'src/info.txt')}))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist/css/'));
-});
+    .pipe(sass({outputStyle: "expanded"})
+      .on("error", function(err) {
+        cb(err)
+      }))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(header({file: path.join(__dirname, "src/info.txt")}))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("dist/css/"))
+})
 
-gulp.task('mincss',(cb)=>{
-  return gulp.src('src/scss/**/*.scss')
-    .pipe(concat('matericious.min.css'))
+task("mincss", (cb) => {
+  return src(dirSass)
+    .pipe(concat("matericious.min.css"))
     .pipe(sourcemaps.init())
-    .pipe(sass({ outputStyle: 'compressed' })
-      .on('error', function (err) { cb(err); }))
-    .pipe(autoprefixer(autoprefixer_options))
-    .pipe(header({file:path.join(__dirname, 'src/info.txt')}))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist/css/'));
-});
+    .pipe(sass({outputStyle: "compressed"})
+      .on("error", function (err) {
+        cb(err)
+      }))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(header({file: path.join(__dirname, "src/info.txt")}))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("dist/css/"))
+})
 
-gulp.task('sass:watch',()=>{
-  gulp.watch('src/scss/**/*.scss', ['scss']);
-});
+task("sass:watch", () => {
+  watch(dirSass, ["scss"])
+})
 
-gulp.task('compilejs',()=>{
-  return gulp.src(['src/js/*.js','!src/js/_base.js'])
-    .pipe(include()).on('error', console.log)
-    .pipe(babel({presets: ['@babel/env']}))
+task("compilejs", () => {
+  return src(["src/js/*.js", "!src/js/_base.js"])
+    .pipe(include())
+    .pipe(babel({presets: ["@babel/env"]}))
     .pipe(strip())
-    .pipe(header({file:path.join(__dirname, 'src/info.txt')}))
-    .pipe(gulp.dest('js'));
-});
+    .pipe(header({file: path.join(__dirname, "src/info.txt")}))
+    .pipe(dest("js"))
+})
 
-gulp.task('combinejs',()=>{
-  return gulp.src(['src/js/_base.js','src/js/*.js'])
+task("combinejs", () => {
+  return src(dirEs)
     .pipe(sourcemaps.init())
-    .pipe(concat('matericious.js'))
-    .pipe(babel({presets: ['@babel/env']}))
+    .pipe(concat("matericious.js"))
+    .pipe(babel({presets: ["@babel/env"]}))
     .pipe(strip())
-    .pipe(header({file:path.join(__dirname, 'src/info.txt')}))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist/js/'));
-});
+    .pipe(header({file: path.join(__dirname, "src/info.txt")}))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("dist/js/"))
+})
 
-gulp.task('minjs',()=>{
-  return gulp.src(['src/js/_base.js','src/js/*.js'])
+task("minjs", () => {
+  return src(dirEs)
     .pipe(sourcemaps.init())
-    .pipe(concat('matericious.min.js'))
-    .pipe(babel({presets: ['@babel/env']}))
+    .pipe(concat("matericious.min.js"))
+    .pipe(babel({presets: ["@babel/env"]}))
     .pipe(uglify())
-    .pipe(header({file:path.join(__dirname, 'src/info.txt')}))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist/js/'));
-});
+    .pipe(header({file: path.join(__dirname, "src/info.txt")}))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("dist/js/"))
+})
 
-gulp.task('js', gulp.series(['compilejs', 'combinejs', 'minjs']));
-gulp.task('css', gulp.series(['compilecss', 'combinecss', 'mincss']));
-gulp.task('default', gulp.series(['css', 'js']));
+task("js", series(["compilejs", "combinejs", "minjs"]))
+task("css", series(["compilecss", "combinecss", "mincss"]))
+task("default", series(["css", "js"]))
