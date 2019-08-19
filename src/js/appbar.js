49023d50d9db831d9c9edd5fa79c9ed30bcf33fd
Kascique Lowmans <kascique.lowmans@gmail.com>
2019-08-19 09:27:29 -0400

    New Appbar and Snackbar component

--- src/js/appbar.js
+++ src/js/appbar.js
@@ -1,63 +1,90 @@
-//=include _base.js
+//=include _utilities.js
 
-function nesting() {
-  const large_screen = 992,
-    medium_screen = 768,
-    small_screen = 500;
-
-  function checkNesting() {
-    let w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    let num_to_nest;
-    let nest_menu = $get("#nesting");
-    let nest_menu_btn = $get('[open-menu="nesting"]');
-    let nest_btns = $all(".appbar .nest");
-    if (w < large_screen) {
-      num_to_nest = 1;
-      if (w <= medium_screen && w >= small_screen) {
-        num_to_nest = 2;
-      } else if (w <= small_screen) {
-        num_to_nest = nest_btns.length;
-      }
-    }
-    nest_btns.forEach(function (nest_btn) {
-      nest_btn.style.display = "block";
-    })
-    nest_menu.innerHTML = "";
-    nest_menu_btn.style.display = "none";
-    nest_btns.forEach(function (nest_btn) {
-      console.log("running");
-      let icon_btn_title = nest_btn.getAttribute("title");
-      let icon_title;
-      if (icon_btn_title != null) {
-        icon_title = ` ${icon_btn_title}`;
-      } else {
-        icon_title = "";
-      }
-      nest_menu_btn.style.display = "inline-block";
-      nest_btn.style.display = "none";
-      nest_menu.innerHTML += `<li><a href="#">${nest_btn.innerHTML + icon_title}</a></li>`;
-    })
+class Appbar extends Utilities {
+  constructor() {
+    super();
+    this.nesting();
+    this.scrollFunc();
   }
 
-  checkNesting();
-    addEvent(window, "resize", function () {
-      checkNesting();
+  nesting() {
+    const large_screen = 992,
+      medium_screen = 768,
+      small_screen = 500;
+
+    let nest_menu = $m("#nesting").element;
+    let nest_menu_btn = $m('[menu="nesting"]').element;
+    let nest_btns = super.$all(".appbar .nest");
+
+    function nest() {
+      let num_to_nest;
+      let w =
+        window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth;
+
+      //console.log("log function called "+large_screen);
+      if (w < large_screen) {
+        num_to_nest = 1;
+        if (w <= medium_screen && w >= small_screen) {
+          num_to_nest = 2;
+        } else if (w <= small_screen) {
+          num_to_nest = nest_btns.length;
+        }
+      }
+      nest_btns.forEach(btn => {
+        btn.style.display = "inline-block";
+      });
+      nest_menu.innerHTML = "";
+      nest_menu_btn.style.display = "none";
+      /*[num_to_nest].forEach((c)=>{
+  
+      });*/
+
+      for (var c = 0; c < num_to_nest; c++) {
+        var icon_btn_title = nest_btns[c].getAttribute("title");
+        var icon_title;
+        if (icon_btn_title != null) {
+          icon_title = " " + icon_btn_title;
+        } else {
+          icon_title = "";
+        }
+
+        nest_menu_btn.style.display = "inline-block";
+        nest_btns[c].style.display = "none";
+        nest_menu.innerHTML += `<li><a href="#">${nest_btns[c].innerHTML +
+          icon_title} </a></li>`;
+      }
+    }
+    nest();
+    addEvent(window, "resize", () => {
+      nest();
     });
   }
 
-  function collapse() {
-    let $curr_scr_pos = window.pageYOffset;
-    if ($get('.dense')) {
-      $get('.dense').style.top = ($pre_Scr_Pos > $curr_scr_pos) ? "0" : "-65px";
-      $pre_Scr_Pos = $curr_scr_pos;
+  scrollFunc() {
+    let curr_scr_pos = window.pageYOffset,
+      pre_Scr_Pos;
+    let dense = $m(".dense").element,
+      collapse = $m(".collapse").element,
+      title = $m(".appbar .title").element;
+
+    function collapse_func() {
+      if ($m(".dense").element) {
+        dense.style.top = pre_Scr_Pos > curr_scr_pos ? "0" : "-65px";
+        pre_Scr_Pos = curr_scr_pos;
+      }
+      if ($m(".collapse").element) {
+        title.style.display = curr_scr_pos < 5 ? "inline-block" : "none";
+        collapse.style.width = curr_scr_pos < 5 ? "100%" : "115px";
+        collapse.style.borderRadius =
+          curr_scr_pos < 5 ? "0" : "0px 0px 30px 0px";
+      }
     }
-    if ($get('.collapse')) {
-      $get('.collapse .title').style.display = ($curr_scr_pos < 5) ? "inline-block" : "none";
-      $get('.collapse').style.width = ($curr_scr_pos < 5) ? "100%" : "115px";
-      $get('.collapse').style.borderRadius = ($curr_scr_pos < 5) ? "0" : "0px 0px 30px 0px";
-    }
+    collapse_func();
+    pre_Scr_Pos = window.pageYOffset;
+    window.onscroll = () => {
+      collapse_func();
+    };
   }
-  let $pre_Scr_Pos = window.pageYOffset;
-  window.onscroll = () => {
-    collapse();
-  }
+}
--- src/js/snackbar.js
+++ src/js/snackbar.js
@@ -1,52 +1,93 @@
-//=include _base.js
+//=include _utilities.js
 
-function Snackbar(data, callback) {
-  let openAni = "slideUpBottom",
-    closeAni = "slideDownBottom";
-  if (!data.pos) {
-    data["pos"] = {
-      vertical: "bottom",
-      horizontal: "center"
+class Snackbar extends Utilities {
+  constructor(message, action, time, callback) {
+    super();
+    this.id = super.gen_ID();
+    this.ani = ["aniOpen", "aniClose"];
+    let speedSnack = {
+      type: action ? "action" : "message",
+      text: message ? message : "",
+      time: time ? time : 5000,
+      actionButton: action ? action : ""
     };
-  }
-  if (!data.theme) data.theme = "dark";
-  if (!data.actionButton) data.actionButton = "";
-  if (data.pos.vertical == "top") {
-    closeAni = "slideDownTop";
-    openAni = "slideUpTop";
-  }
-  let snackbarElem = $get("snackbar");
-  snackbarElem.innerHTML = snackbarElem.className = "";
-  let snackbarClass = `${data.type} ${data.pos.vertical} ${data.pos.horizontal} ${data.theme}`;
-  snackbarElem.className += snackbarClass;
-  let snackbarHTML = `<div class="snackbar">
-      <text class="text">${data.text}</text>
-      <button class="ripple SnackClose"><i class="material-icons">close</i></button>
-      <button class="ripple SnackAction">${data.actionButton}</button>
-    </div>`;
-  snackbarElem.innerHTML = snackbarHTML;
-  $get(".SnackClose").addEventListener("click", function () {
-    callback(false);
-    close();
-  });
-  $get(".SnackAction").addEventListener("click", function () {
-    callback(true);
-    close();
-  });
-  $get(".snackbar").className += " " + openAni;
 
-  function timer(time) {
-    let timer;
+    if (message) {
+      this.create(speedSnack, callback);
+      this.show();
+    }
+  }
+
+  message() {
+    alert("welcome");
+  }
+
+  create(data, callback) {
+    this.data = {
+      ID: this.id,
+      class: "." + this.id,
+      type: !data.type ? "message" : data.type,
+      text: data.text,
+      time: !data.time ? null : data.time,
+      theme: !data.theme ? "dark" : data.theme,
+      actionButton: !data.actionButton ? "" : data.actionButton,
+      vert: !data.vert ? "bottom" : data.vert,
+      hori: !data.hori ? "left" : data.hori
+    };
+
+    callback = !callback ? d => {} : callback;
+    const snack = this.data;
+    let snack_div = super.create("div");
+    let snack_class_array = `${snack.type} ${snack.vert} ${snack.hori} ${
+      snack.theme
+    }`.split(" ");
+
+    snack_div.className = "snackbar " + snack.ID;
+    const snack_elem = `<div>
+      <text class="text">${snack.text}</text>
+      <button class="ripple SnackClose"><i class="material-icons">close</i></button>
+      <button class="ripple SnackAction">${snack.actionButton}</button>
+    </div>`;
+
+    document.body.appendChild(snack_div);
+    $m(snack.class).html(snack_elem);
+
+    snack_class_array.forEach(_class => {
+      $m(snack.class).class(_class);
+    });
+
+    $m(`${this.data.class} .SnackClose`).when("click", () => {
+      callback(false);
+      this.close();
+    });
+    $m(`${this.data.class} .SnackAction`).when("click", () => {
+      callback(true);
+      this.close();
+    });
+
+    this.timer(snack.time);
+  }
+
+  show() {
+    $m(`${this.data.class}`).class(this.ani[0]);
+  }
+
+  timer(time) {
+    this.timer;
     if (time != null) {
-      timer = setInterval(function () {
-        clearInterval(timer);
-        close();
+      this.timer = setInterval(() => {
+        clearInterval(this.timer);
+        this.close();
       }, time);
     }
   }
 
-  function close() {
-    $get(".snackbar").className += ` ${closeAni}`;
+  close(close_class) {
+    $m(this.data.class).class(this.ani[0]);
+    $m(this.data.class).class(this.ani[1]);
+    setTimeout(() => {
+      $m(this.data.class).delete();
+    }, 500);
+    clearInterval(this.timer);
   }
-  timer(data.time);
 }
